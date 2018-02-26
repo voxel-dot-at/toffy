09db9bf7f1bfdf7cbc81c78057fb9ba94d542888
Simon Vogl <simon@voxel.at>
2018-02-26 20:45:33 +0100

    refine ocv version for tracker workaround

--- modules/filters/src/tracking/cvTracker.cpp
+++ modules/filters/src/tracking/cvTracker.cpp
@@ -132,7 +132,7 @@ bool CVTracker::filter(const Frame& in, Frame& out) {
 
     if(!tracker) {
         bbox = cv::minAreaRect(detObj->at(0)->contour).boundingRect();
-#if OCV_VERSION_MAJOR >= 3
+#if (OCV_VERSION_MAJOR >= 3 ) && (OCV_VERSION_MINOR > 1)
         tracker = cv::TrackerKCF::create( );
 #else
         tracker = cv::Tracker::create( "KCF" );
