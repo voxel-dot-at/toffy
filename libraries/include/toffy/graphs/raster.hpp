405ebb2026e1cc0abd05dbc66de05278d919a7d5
Simon Vogl <simon@voxel.at>
2021-02-09 15:20:16 +0100

    added missing copyright

--- libraries/include/toffy/graphs/raster.hpp
+++ libraries/include/toffy/graphs/raster.hpp
@@ -1,5 +1,19 @@
-#ifndef RASTER_H_
-#define RASTER_H_
+/*
+   Copyright 2012-2021 Simon Vogl <svogl@voxel.at> VoXel Interaction Design - www.voxel.at
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+#pragma once
 
 #include <toffy/toffy_export.h>
 
@@ -293,5 +307,3 @@ extern void paintSegment(cv::Mat& disMax, const RasterSegment& seg,
 
 std::ostream& operator<<(std::ostream& o, const RasterPoint& pt );
 std::ostream& operator<<(std::ostream& o, const RasterSegment& seg );
-
-#endif
--- libraries/sensor/include/toffy/imagesensor.hpp
+++ libraries/sensor/include/toffy/imagesensor.hpp
@@ -1,5 +1,19 @@
-#ifndef __IMAGESENSOR_HPP__
-#define __IMAGESENSOR_HPP__
+/*
+   Copyright 2012-2021 Simon Vogl <svogl@voxel.at> VoXel Interaction Design - www.voxel.at
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+#pragma once
 
 #if defined(MSVC)
 	#define DLLExport __declspec( dllexport )
@@ -27,6 +41,3 @@ class DLLExport ImageSensor: public Sensor
 	virtual int getFrame(cv::Mat &image)=0;
 
 };
-
-
-#endif //__IMAGESENSOR_HPP__
