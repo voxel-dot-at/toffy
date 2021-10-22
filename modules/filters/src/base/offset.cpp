ad7fbd4e692178d292e9eb9f912127bd7d7181fa
Simon Vogl <simon.vogl@ktm.com>
2021-10-22 09:34:01 +0200

    code cleanup

--- modules/filters/src/base/offset.cpp
+++ modules/filters/src/base/offset.cpp
@@ -14,51 +14,49 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-#include <string>
-#include <iostream>
+
 #include <fstream>
+#include <iostream>
+#include <string>
 #include <limits.h>
 
 #include <boost/log/trivial.hpp>
+#include <boost/foreach.hpp>
 //#include <boost/filesystem.hpp>
 #include <boost/algorithm/string/case_conv.hpp>
-#include <boost/foreach.hpp>
-
-#include <boost/date_time/posix_time/posix_time_types.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/date_time/posix_time/posix_time_types.hpp>
 
 #include "toffy/base/offset.hpp"
-
-
 
 using namespace toffy;
 using namespace toffy::filters;
 using namespace cv;
 using namespace std;
 namespace logging = boost::log;
-//namespace fs = boost::filesystem;
 
 #ifdef CM_DEBUG
-const bool dbg=true;
+statics const bool dbg = true;
 #else
-const bool dbg=false;
+statics const bool dbg = false;
 #endif
 
 std::size_t OffSet::_filter_counter = 1;
 const std::string OffSet::id_name = "offset";
 
-OffSet::OffSet(): Filter(OffSet::id_name,_filter_counter),
-		_sumValue(0.0),
-		_mulValue(1.0),
-		_in_img("img")
-{
-    _filter_counter++;
+OffSet::OffSet()
+    : Filter(OffSet::id_name, _filter_counter),
+      _sumValue(0.0),
+      _mulValue(1.0),
+      _in_img("img") {
+  _filter_counter++;
 }
 
 /*
 int OffSet::loadConfig(const boost::property_tree::ptree& pt) {
-    BOOST_LOG_TRIVIAL(debug) << __FUNCTION__ << "(const boost::property_tree::ptree& pt)";
-    const boost::property_tree::ptree& tree = pt.get_child(this->type());
+    BOOST_LOG_TRIVIAL(debug) << __FUNCTION__ << "(const
+boost::property_tree::ptree& pt)"; const boost::property_tree::ptree& tree =
+pt.get_child(this->type());
 
     loadGlobals(tree);
 
@@ -69,86 +67,91 @@ int OffSet::loadConfig(const boost::property_tree::ptree& pt) {
 */
 
 void OffSet::updateConfig(const boost::property_tree::ptree &pt) {
-    BOOST_LOG_TRIVIAL(debug) << __FUNCTION__ <<  " " << id();
+  BOOST_LOG_TRIVIAL(debug) << __FUNCTION__ << " " << id();
 
-    using namespace boost::property_tree;
+  using namespace boost::property_tree;
 
-    Filter::updateConfig(pt);
+  Filter::updateConfig(pt); // sets log level and other common settings
 
-    _in_img = pt.get<string>("inputs.img",_in_img);
+  _in_img = pt.get<string>("inputs.img", _in_img);
 
-    _sumValue = pt.get<float>("options.sumValue",_sumValue);
-    _mulValue = pt.get<float>("options.mulValue",_mulValue);
+  _sumValue = pt.get<float>("options.sumValue", _sumValue);
+  _mulValue = pt.get<float>("options.mulValue", _mulValue);
 
-    _out_img = pt.get<string>("outputs.img", _out_img);
+  _out_img = pt.get<string>("outputs.img", _out_img);
 
-    try {
-	BOOST_FOREACH(const ptree::value_type &v, pt.get_child("options.roi")) {
-	    Rect roi(0,0,0,0);
-	    roi.x = v.second.get<int>("x",roi.x);
-	    roi.y = v.second.get<int>("y",roi.y);
-	    roi.width = v.second.get<int>("width",roi.width);
-	    roi.height = v.second.get<int>("height",roi.height);
-	    _rois.push_back(roi);
-	}
-    } catch (const std::exception& ex) {
-	BOOST_LOG_TRIVIAL(debug) << "no roi found. Using full image.";
+  try {
+    BOOST_FOREACH (const ptree::value_type &v, pt.get_child("options.roi")) {
+      Rect roi(0, 0, 0, 0);
+      roi.x = v.second.get<int>("x", roi.x);
+      roi.y = v.second.get<int>("y", roi.y);
+      roi.width = v.second.get<int>("width", roi.width);
+      roi.height = v.second.get<int>("height", roi.height);
+      _rois.push_back(roi);
     }
+  } catch (const std::exception &ex) {
+    BOOST_LOG_TRIVIAL(debug) << "no roi found. Using full image.";
+  }
 }
 
-bool OffSet::filter(const Frame &in, Frame& out) const {
-	BOOST_LOG_TRIVIAL(debug) << __FUNCTION__;
-    using namespace boost::posix_time;
+bool OffSet::filter(const Frame &in, Frame &out) const {
+  LOG(debug) << __FUNCTION__;
+  using namespace boost::posix_time;
 
-    auto start = microsec_clock::local_time();
-    boost::posix_time::time_duration diff;
+  auto start = microsec_clock::local_time();
+  boost::posix_time::time_duration diff;
 
+  if (_rois.size() == 0) {
+    LOG(debug) << "depth found";
+    matPtr d = in.getMatPtr(_in_img);
+    matPtr o;
 
-//	out = in;
+    if (out.hasKey(_out_img))
+      o = boost::any_cast<matPtr>(in.getData(_out_img));
+    else {
+      o.reset(new Mat());
+      out.addData(_out_img, o);
+    }
 
-	if (_rois.size() == 0) {
-		BOOST_LOG_TRIVIAL(debug) << "depth found";
-		boost::shared_ptr<cv::Mat> d = in.getMatPtr(_in_img);
-		*d += _sumValue;
-		*d *= _mulValue;
-	} else {
+    *o = *d + _sumValue;
+    *o *= _mulValue;
 
-		for(size_t i = 0; i < _rois.size(); i++) {
-			boost::shared_ptr<cv::Mat> d = in.getMatPtr(_in_img);
-			Mat roi((*d)(_rois[i]));
-			roi += _sumValue;
-			roi *= _mulValue;
-		}
-	}
+  } else {
+    for (size_t i = 0; i < _rois.size(); i++) {
+      boost::shared_ptr<cv::Mat> d = in.getMatPtr(_in_img);
+      Mat roi((*d)(_rois[i]));
+      roi += _sumValue;
+      roi *= _mulValue;
+    }
+  }
 
-	    diff = boost::posix_time::microsec_clock::local_time() - start;
-    BOOST_LOG_TRIVIAL(debug) << "OffSet::filter: " << diff.total_microseconds();
+  diff = boost::posix_time::microsec_clock::local_time() - start;
+  BOOST_LOG_TRIVIAL(debug) << "OffSet::filter: " << diff.total_microseconds();
 
-	return true;
+  return true;
 }
 
 void OffSet::addRoi(cv::Rect newRoi) {
-	BOOST_LOG_TRIVIAL(debug) << __FUNCTION__;
-	_rois.push_back(newRoi);
+  BOOST_LOG_TRIVIAL(debug) << __FUNCTION__;
+  _rois.push_back(newRoi);
 }
 
-boost::property_tree::ptree OffSet::getConfig() const
-{
-    boost::property_tree::ptree pt, opt;
+boost::property_tree::ptree OffSet::getConfig() const {
+  boost::property_tree::ptree pt, opt;
 
-    pt = Filter::getConfig();
+  pt = Filter::getConfig();
 
-    opt.put("sumValue", _sumValue);
-    opt.put("mulValue", _mulValue);
-    pt.add_child("options", opt);
-    opt.clear();
+  opt.put("sumValue", _sumValue);
+  opt.put("mulValue", _mulValue);
+  pt.add_child("options", opt);
+  opt.clear();
 
-    opt.put("img", _in_img);
-    pt.add_child("inputs", opt);
-    opt.clear();
+  opt.put("img", _in_img);
+  pt.add_child("inputs", opt);
+  opt.clear();
 
-    opt.put("img", _out_img);
-    pt.add_child("outputs", opt);
+  opt.put("img", _out_img);
+  pt.add_child("outputs", opt);
 
-    return pt;
+  return pt;
 }
