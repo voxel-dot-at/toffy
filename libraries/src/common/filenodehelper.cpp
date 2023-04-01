3ebed9cbd8f94c677ccc6594613e9e6b623aaac8
Simon Vogl <simon.vogl@ktm.com>
2023-04-01 16:35:00 +0200

    cosmetics

--- libraries/src/common/filenodehelper.cpp
+++ libraries/src/common/filenodehelper.cpp
@@ -6,7 +6,6 @@
 #include <boost/log/trivial.hpp>
 #include <iostream>
 
-
 using namespace toffy;
 using namespace commons;
 
@@ -15,37 +14,35 @@ using namespace boost::property_tree;
 
 using namespace std;
 
-  cv::FileStorage toffy::commons::loadOCVnode(const boost::property_tree::ptree& pt) {
+cv::FileStorage toffy::commons::loadOCVnode(
+    const boost::property_tree::ptree& pt)
+{
     BOOST_LOG_TRIVIAL(debug) << __FUNCTION__;
     std::ostringstream oss;
 
     using namespace boost::property_tree::xml_parser;
 
     boost::property_tree::ptree oc;
-    //cout << pt.data() << endl;
+
     oc.add_child("opencv_storage", pt);
-    /*#if (BOOST_VERSION > 105500 )
-        boost::property_tree::xml_parser::xml_writer_settings<std::string> settings('\t', 1);
-    #else
-        boost::property_tree::xml_parser::xml_writer_settings<char> settings('\t', 1);
-    #endif*/
-    //boost::property_tree::xml_parser::write_xml(oss, oc, settings);
+
     xml_parser::write_xml(oss, oc);
-    //cout << "oss.str(): " << oss.str() << endl;
+
     BOOST_LOG_TRIVIAL(debug) << oss.str();
 
-    cv::FileStorage fs( oss.str() , cv::FileStorage::MEMORY | cv::FileStorage::READ );
+    cv::FileStorage fs(oss.str(),
+                       cv::FileStorage::MEMORY | cv::FileStorage::READ);
 
     return fs;
-  }
+}
 
-
-  bool toffy::commons::checkOCVNone(const boost::property_tree::ptree& pt) {
+bool toffy::commons::checkOCVNone(const boost::property_tree::ptree& pt)
+{
     BOOST_LOG_TRIVIAL(debug) << __FUNCTION__;
-    //cout << pt.data() << endl;
+
     boost::optional<string> attr = pt.get_optional<string>("<xmlattr>.type_id");
     if (attr.is_initialized() && *attr == "opencv-matrix") {
-	return true;
+        return true;
     }
     return false;
-  }
+}
--- modules/filters/src/smoothing/kalmanaverage.cpp
+++ modules/filters/src/smoothing/kalmanaverage.cpp
@@ -18,11 +18,11 @@
 #include <boost/log/trivial.hpp>
 
 #if OCV_VERSION_MAJOR >= 3
-#  include <opencv2/imgproc.hpp>
-#  include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
 #else
-#  include <opencv2/imgproc/imgproc.hpp>
-#  include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #endif
 
 #include "toffy/smoothing/kalmanaverage.hpp"
@@ -32,19 +32,21 @@
 
 #include <math.h>
 
-
 using namespace std;
 using namespace cv;
 using namespace toffy::filters::smoothing;
 
 std::size_t toffy::filters::smoothing::KalmanAverage::_filter_counter = 1;
-const std::string toffy::filters::smoothing::KalmanAverage::id_name = "kalmanAverage";
+const std::string toffy::filters::smoothing::KalmanAverage::id_name =
+    "kalmanAverage";
 
-KalmanAverage::KalmanAverage():
-    Filter(KalmanAverage::id_name), _in_img("depth"), out_img("depth"),
-    processNoiseCov(1e-5),
-    measurementNoiseCov(1e-1),
-    skipZeros(true)
+KalmanAverage::KalmanAverage()
+    : Filter(KalmanAverage::id_name),
+      _in_img("depth"),
+      out_img("depth"),
+      processNoiseCov(1e-5),
+      measurementNoiseCov(1e-1),
+      skipZeros(true)
 {
 }
 
@@ -52,8 +54,9 @@ KalmanAverage::~KalmanAverage() {}
 
 /*
 int KalmanAverage::loadConfig(const boost::property_tree::ptree& pt) {
-    BOOST_LOG_TRIVIAL(debug) << __FUNCTION__ << "(const boost::property_tree::ptree& pt)";
-    const boost::property_tree::ptree& tree = pt.get_child(this->type());
+    BOOST_LOG_TRIVIAL(debug) << __FUNCTION__ << "(const
+boost::property_tree::ptree& pt)"; const boost::property_tree::ptree& tree =
+pt.get_child(this->type());
 
     loadGlobals(tree);
 
@@ -63,22 +66,25 @@ int KalmanAverage::loadConfig(const boost::property_tree::ptree& pt) {
 }
 */
 
-void KalmanAverage::updateConfig(const boost::property_tree::ptree &pt) {
-    BOOST_LOG_TRIVIAL(debug) << __FUNCTION__ <<  " " << id();
+void KalmanAverage::updateConfig(const boost::property_tree::ptree& pt)
+{
+    BOOST_LOG_TRIVIAL(debug) << __FUNCTION__ << " " << id();
 
     using namespace boost::property_tree;
 
     Filter::updateConfig(pt);
 
-    _in_img = pt.get<string>("inputs.img",_in_img);
-    out_img = pt.get<string>("outputs.img",out_img);
+    _in_img = pt.get<string>("inputs.img", _in_img);
+    out_img = pt.get<string>("outputs.img", out_img);
 
-     processNoiseCov = pt.get<float>("options.processNoiseCov", processNoiseCov);
-     measurementNoiseCov = pt.get<float>("options.measurementNoiseCov", measurementNoiseCov);
-     skipZeros = pt.get<bool>("options.skipZeros", skipZeros);
+    processNoiseCov = pt.get<float>("options.processNoiseCov", processNoiseCov);
+    measurementNoiseCov =
+        pt.get<float>("options.measurementNoiseCov", measurementNoiseCov);
+    skipZeros = pt.get<bool>("options.skipZeros", skipZeros);
 }
 
-boost::property_tree::ptree KalmanAverage::getConfig() const {
+boost::property_tree::ptree KalmanAverage::getConfig() const
+{
     boost::property_tree::ptree pt;
 
     pt = Filter::getConfig();
@@ -93,86 +99,85 @@ boost::property_tree::ptree KalmanAverage::getConfig() const {
     return pt;
 }
 
-bool KalmanAverage::filter(const toffy::Frame& in, toffy::Frame& out)
+bool KalmanAverage::filter(const toffy::Frame& in, toffy::Frame& /*out*/)
 {
-    BOOST_LOG_TRIVIAL(debug) << __FUNCTION__ <<  " " << id();
+    BOOST_LOG_TRIVIAL(debug) << __FUNCTION__ << " " << id();
     boost::shared_ptr<cv::Mat> img;
 
     try {
-	img = boost::any_cast<boost::shared_ptr<cv::Mat> >(in.getData(_in_img));
-    } catch(const boost::bad_any_cast &) {
-	BOOST_LOG_TRIVIAL(warning) <<
-	    "Could not cast input " << _in_img <<
-	    ", filter  " << id() <<" not applied.";
-	return false;
+        img = boost::any_cast<boost::shared_ptr<cv::Mat> >(in.getData(_in_img));
+    } catch (const boost::bad_any_cast&) {
+        BOOST_LOG_TRIVIAL(warning) << "Could not cast input " << _in_img
+                                   << ", filter  " << id() << " not applied.";
+        return false;
     }
     float* imgptr;
     int vKFpos = 0;
     if (!vKF) {
-	vKF.reset(new std::vector<cv::KalmanFilter >(img->size().area()));
-	for(int row = 0; row < img->rows; ++row) {
-	    imgptr = img->ptr<float>(row);
+        vKF.reset(new std::vector<cv::KalmanFilter>(img->size().area()));
+        for (int row = 0; row < img->rows; ++row) {
+            imgptr = img->ptr<float>(row);
 
-	    for(int col = 0; col < img->cols; ++col) {
-		cv::KalmanFilter& kf = vKF->at(vKFpos);
-		//vKFpos = ( ( (row+1)+((img->rows)*row) ) * (col+1) ) -1;
-		kf.init(2,1,0);
-		kf.transitionMatrix = (Mat_<float>(2, 2) << 1, 1, 0, 1);
+            for (int col = 0; col < img->cols; ++col) {
+                cv::KalmanFilter& kf = vKF->at(vKFpos);
+                // vKFpos = ( ( (row+1)+((img->rows)*row) ) * (col+1) ) -1;
+                kf.init(2, 1, 0);
+                kf.transitionMatrix = (Mat_<float>(2, 2) << 1, 1, 0, 1);
 
-		setIdentity(kf.measurementMatrix);
-		setIdentity(kf.processNoiseCov, Scalar::all(1e-5));
-		setIdentity(kf.measurementNoiseCov, Scalar::all(1e-1));
-		setIdentity(kf.errorCovPost, Scalar::all(1));
+                setIdentity(kf.measurementMatrix);
+                setIdentity(kf.processNoiseCov, Scalar::all(1e-5));
+                setIdentity(kf.measurementNoiseCov, Scalar::all(1e-1));
+                setIdentity(kf.errorCovPost, Scalar::all(1));
 
-		kf.statePost.at<float>(0) = imgptr[col];
-		vKFpos++;
-		//cout << "vKFpos: " << vKFpos << endl;
-		//cout << "imgptr[col]: " << imgptr[col] << endl;
-		//cout << "kf.statePost.at<float>(0): " << kf.statePost.at<float>(0) << endl;
-	    }
-	}
+                kf.statePost.at<float>(0) = imgptr[col];
+                vKFpos++;
+                // cout << "vKFpos: " << vKFpos << endl;
+                // cout << "imgptr[col]: " << imgptr[col] << endl;
+                // cout << "kf.statePost.at<float>(0): " <<
+                // kf.statePost.at<float>(0) << endl;
+            }
+        }
     } else {
-	Mat measurement = Mat::zeros(1, 1, CV_32F);
-	Mat estimated;
-	for(int row = 0; row < img->rows; ++row) {
-	    imgptr = img->ptr<float>(row);
+        Mat measurement = Mat::zeros(1, 1, CV_32F);
+        Mat estimated;
+        for (int row = 0; row < img->rows; ++row) {
+            imgptr = img->ptr<float>(row);
 
-	    for(int col = 0; col < img->cols; ++col) {
-		cv::KalmanFilter& kf = vKF->at(vKFpos);
+            for (int col = 0; col < img->cols; ++col) {
+                cv::KalmanFilter& kf = vKF->at(vKFpos);
 
-		if (std::numeric_limits<float>::quiet_NaN() == imgptr[col]
-		    || ( skipZeros && imgptr[col] <= 0.0 ) ) {
-		    imgptr[col] = imgptr[col]; // nothing to estimate..
-		    vKFpos++;
+                if (std::numeric_limits<float>::quiet_NaN() == imgptr[col] ||
+                    (skipZeros && imgptr[col] <= 0.0)) {
+                    imgptr[col] = imgptr[col];  // nothing to estimate..
+                    vKFpos++;
 
-		    //kf.statePost.at<float>(0) =imgptr[col] ;
-		    continue;
-		}
-		// last state was nan..:
-		if (  std::numeric_limits<float>::quiet_NaN() == kf.statePost.at<float>(0) ) {
-		    // re-init state
-		    setIdentity(kf.measurementMatrix);
-		    setIdentity(kf.processNoiseCov, Scalar::all(1e-5));
-		    setIdentity(kf.measurementNoiseCov, Scalar::all(1e-1));
-		    setIdentity(kf.errorCovPost, Scalar::all(1));
-		    
-		    kf.statePost.at<float>(0) = imgptr[col];
-		}
+                    // kf.statePost.at<float>(0) =imgptr[col] ;
+                    continue;
+                }
+                // last state was nan..:
+                if (std::numeric_limits<float>::quiet_NaN() ==
+                    kf.statePost.at<float>(0)) {
+                    // re-init state
+                    setIdentity(kf.measurementMatrix);
+                    setIdentity(kf.processNoiseCov, Scalar::all(1e-5));
+                    setIdentity(kf.measurementNoiseCov, Scalar::all(1e-1));
+                    setIdentity(kf.errorCovPost, Scalar::all(1));
 
-		//vKFpos = ( ( (row+1)+((img->rows)*row) ) * (col+1) ) -1;
-		kf.predict();
+                    kf.statePost.at<float>(0) = imgptr[col];
+                }
 
-		//measurement = Mat::zeros(3, 1, CV_32F);
-		measurement.at<float>(0) = imgptr[col];
+                // vKFpos = ( ( (row+1)+((img->rows)*row) ) * (col+1) ) -1;
+                kf.predict();
 
-		estimated = kf.correct(measurement);
-		imgptr[col] = estimated.at<float>(0);
-		vKFpos++;
-	    }
-	}
+                // measurement = Mat::zeros(3, 1, CV_32F);
+                measurement.at<float>(0) = imgptr[col];
+
+                estimated = kf.correct(measurement);
+                imgptr[col] = estimated.at<float>(0);
+                vKFpos++;
+            }
+        }
     }
-
 
     return true;
 }
-
