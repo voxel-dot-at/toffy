35640521789410a841daca37a4bdd4f9faf28b0a
Simon Vogl <simon.vogl@voxel.at>
2023-10-24 09:02:40 +0200

    cosmetics

--- modules/filters/src/viewers/colorize.cpp
+++ modules/filters/src/viewers/colorize.cpp
@@ -14,13 +14,9 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-#if OCV_VERSION_MAJOR >= 3
-#  include <opencv2/imgproc.hpp>
-#  include <opencv2/highgui.hpp>
-#else
-#  include <opencv2/imgproc/imgproc.hpp>
-#  include <opencv2/highgui/highgui.hpp>
-#endif
+
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
 
 #include <boost/log/trivial.hpp>
 #include <boost/any.hpp>
@@ -38,46 +34,52 @@ using namespace std;
 std::size_t Colorize::filter_counter = 1;
 const std::string Colorize::id_name = "colorize";
 
-Colorize::Colorize(): Filter(Colorize::id_name,filter_counter),
-		      scale(1.f), max(1.5), min(0.01), in_img("depth"),out_img("colored"), colormap("jet")
+Colorize::Colorize()
+    : Filter(Colorize::id_name, filter_counter),
+      scale(1.f),
+      max(1.5),
+      min(0.01),
+      in_img("depth"),
+      out_img("colored"),
+      colormap("jet")
 {
     filter_counter++;
 }
 
-Colorize::~Colorize(){
-}
+Colorize::~Colorize() {}
 
-void Colorize::updateConfig(const boost::property_tree::ptree &pt)
+void Colorize::updateConfig(const boost::property_tree::ptree& pt)
 {
-    BOOST_LOG_TRIVIAL(debug) << __FUNCTION__ <<  " " << id();
+    BOOST_LOG_TRIVIAL(debug) << __FUNCTION__ << " " << id();
 
     using namespace boost::property_tree;
 
     Filter::updateConfig(pt);
 
-    scale = pt.get<double>("options.scale",scale);
-    max = pt.get<double>("options.max",max);
-    min = pt.get<double>("options.min",min);
+    scale = pt.get<double>("options.scale", scale);
+    max = pt.get<double>("options.max", max);
+    min = pt.get<double>("options.min", min);
 
-    colormap = pt.get<string>("options.colormap",colormap);
+    colormap = pt.get<string>("options.colormap", colormap);
 
-    in_img = pt.get<string>("inputs.img",in_img);
-    out_img = pt.get<string>("outputs.img",out_img);
+    in_img = pt.get<string>("inputs.img", in_img);
+    out_img = pt.get<string>("outputs.img", out_img);
 
-    if ( colormap == "jet" ) {
-	colormap_value = COLORMAP_JET;
-    } else if ( colormap == "hot" ) {
-	colormap_value = COLORMAP_HOT;
-    } else if ( colormap == "hsv" ) {
-	colormap_value = COLORMAP_HSV;
-    } else if ( colormap == "rainbow" ) {
-	colormap_value = COLORMAP_RAINBOW;
+    if (colormap == "jet") {
+        colormap_value = COLORMAP_JET;
+    } else if (colormap == "hot") {
+        colormap_value = COLORMAP_HOT;
+    } else if (colormap == "hsv") {
+        colormap_value = COLORMAP_HSV;
+    } else if (colormap == "rainbow") {
+        colormap_value = COLORMAP_RAINBOW;
     } else {
-	colormap_value = COLORMAP_JET;
+        colormap_value = COLORMAP_JET;
     }
 }
 
-boost::property_tree::ptree Colorize::getConfig() const {
+boost::property_tree::ptree Colorize::getConfig() const
+{
     boost::property_tree::ptree pt;
 
     pt = Filter::getConfig();
@@ -94,7 +96,7 @@ boost::property_tree::ptree Colorize::getConfig() const {
     return pt;
 }
 
-bool Colorize::filter(const Frame &in, Frame& out)
+bool Colorize::filter(const Frame& in, Frame& out)
 {
     matPtr img, colored;
 
@@ -105,29 +107,28 @@ bool Colorize::filter(const Frame &in, Frame& out)
 
     BOOST_LOG_TRIVIAL(debug) << __FUNCTION__ << " " << id();
 
-    ///// INPUT 
+    ///// INPUT
 
-    if ( ! in.hasKey(in_img)) {
-        BOOST_LOG_TRIVIAL(warning) << 	"Could not find input " << in_img <<
-        ", filter  " << id() <<" not applied.";
+    if (!in.hasKey(in_img)) {
+        BOOST_LOG_TRIVIAL(warning) << "Could not find input " << in_img
+                                   << ", filter  " << id() << " not applied.";
         return false;
     }
     img = in.getMatPtr(in_img);
-   
+
     Mat& inp = *img;
     if (!inp.data) {
-      BOOST_LOG_TRIVIAL(warning) <<
-	"missing image " << in_img <<
-	", filter  " << id() <<" not applied.";
-      return false;
+        BOOST_LOG_TRIVIAL(warning) << "missing image " << in_img << ", filter  "
+                                   << id() << " not applied.";
+        return false;
     }
 
-    if (! out.hasKey(out_img)) {
-	colored.reset(new Mat(Size(img->rows*scale, img->cols*scale), CV_8UC3)); 
+    if (!out.hasKey(out_img)) {
+        colored.reset(
+            new Mat(Size(img->rows * scale, img->cols * scale), CV_8UC3));
     } else {
-	colored = out.getMatPtr(out_img);
+        colored = out.getMatPtr(out_img);
     }
-
 
     //// PROCESSING
 
@@ -142,29 +143,30 @@ bool Colorize::filter(const Frame &in, Frame& out)
 
     //show.convertTo(show,CV_8U,255.0/(1000-0),-255.0*0/(1000-0));
     if (max == min) {
-	minMaxIdx(inp, &min, &max);
+        minMaxIdx(inp, &min, &max);
     }
-    inp.convertTo(gray,CV_8U,255.0/(max-min),-255.0*min/(max-min));
+    inp.convertTo(gray, CV_8U, 255.0 / (max - min), -255.0 * min / (max - min));
 
     diff = boost::posix_time::microsec_clock::local_time() - start;
-    BOOST_LOG_TRIVIAL(debug) << "Colorize gray2rgb: " << diff.total_microseconds();
+    BOOST_LOG_TRIVIAL(debug)
+        << "Colorize gray2rgb: " << diff.total_microseconds();
 
     // color conversion...:
     applyColorMap(gray, col, colormap_value);
 
-    bounds = (inp < min) | ( inp > max) | ( inp != inp );
-    col.setTo(Scalar(0,0,0), bounds);
+    bounds = (inp < min) | (inp > max) | (inp != inp);
+    col.setTo(Scalar(0, 0, 0), bounds);
 
+    if (scale != 1.f) {
+        int method = scale < 1 ? INTER_AREA : INTER_LINEAR;
+        resize(col, *colored, Size(), scale, scale, method);
 
-    if( scale != 1.f ) {
-      int method = scale < 1 ? INTER_AREA : INTER_LINEAR;
-      resize(col, *colored, Size(), scale, scale, method);
-
-      diff = boost::posix_time::microsec_clock::local_time() - start;
-      BOOST_LOG_TRIVIAL(debug) << "Colorize resize: " << diff.total_microseconds();
+        diff = boost::posix_time::microsec_clock::local_time() - start;
+        BOOST_LOG_TRIVIAL(debug)
+            << "Colorize resize: " << diff.total_microseconds();
 
     } else {
-	*colored = col;
+        *colored = col;
     }
 
     //imshow(name() + " " + out_img, col);
